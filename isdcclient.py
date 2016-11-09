85fc750dc1cfa88d79e25a54f5df15e0599d16c0
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2016-11-09 16:16:42 +0100

    delays

--- isdcclient.py
+++ isdcclient.py
@@ -7,10 +7,14 @@ import numpy as np
 isdc_server="isdc.unige.ch"
 
 timeout=300
+num_max_retries=10
+retry_delay=10
 
 
 def genlc(target,utc,span):
-    return requests.get("http://isdc.unige.ch/~savchenk/spiacs-online/spiacs.pl", params={"requeststring": target+" "+utc+" "+span,'submit':"Submit", 'generate':'genlc'},timeout=timeout).content
+    return requests.get("http://isdc.unige.ch/~savchenk/spiacs-online/spiacs.pl", params={"requeststring": target+" "+utc+" "+span,'submit':"Submit", 'generate':'genlc'},
+            max_retries=num_max_retries, delay_between_retries=retry_delay,timeout=timeout).content
 
 def getscw(intime):
-    return requests.get("http://isdc.unige.ch/~savchenk/spiacs-online/spiacs.pl?requeststring="+intime,params=dict(generate="scw",submit="Submit"),timeout=timeout).content[:12]
+    return requests.get("http://isdc.unige.ch/~savchenk/spiacs-online/spiacs.pl?requeststring="+intime,params=dict(generate="scw",submit="Submit"),
+            max_retries=num_max_retries, dely_between_retries=3,timeout=timeout).content[:12]
