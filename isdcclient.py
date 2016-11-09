61eb7761ff455b23264754479407d294e361ba82
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2016-11-09 18:53:50 +0100

    delays and retries

--- isdcclient.py
+++ isdcclient.py
@@ -11,10 +11,23 @@ num_max_retries=10
 retry_delay=10
 
 
+import requests
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
+
+def get_with_retries(url,params):
+    s = requests.Session()
+
+    retries = Retry(total=5,
+                    backoff_factor=0.1,
+                    status_forcelist=[ 500, 502, 503, 504 ])
+
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+
+    return s.get(url,params=params)
+
 def genlc(target,utc,span):
-    return requests.get("http://isdc.unige.ch/~savchenk/spiacs-online/spiacs.pl", params={"requeststring": target+" "+utc+" "+span,'submit':"Submit", 'generate':'genlc'},
-            max_retries=num_max_retries, delay_between_retries=retry_delay,timeout=timeout).content
+    return get_with_retries("http://isdc.unige.ch/~savchenk/spiacs-online/spiacs.pl", params={"requeststring": target+" "+utc+" "+span,'submit':"Submit", 'generate':'genlc'}).content
 
 def getscw(intime):
-    return requests.get("http://isdc.unige.ch/~savchenk/spiacs-online/spiacs.pl?requeststring="+intime,params=dict(generate="scw",submit="Submit"),
-            max_retries=num_max_retries, dely_between_retries=3,timeout=timeout).content[:12]
+    return get_with_retries("http://isdc.unige.ch/~savchenk/spiacs-online/spiacs.pl?requeststring="+intime,params=dict(generate="scw",submit="Submit")).content[:12]
